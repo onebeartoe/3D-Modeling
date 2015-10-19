d81c740b63c73809d7809c82a88fe5e78d28b727
onebeartoe <onebeartoe@gmail.com>
2015-10-19 15:00:00 -0500

    The body of the script was put into it won function.

--- OpenSCAD/find-cleanup-items.sh
+++ OpenSCAD/find-cleanup-items.sh
@@ -1,20 +1,30 @@
 
-# pipe all proposed baseline PNG file names to the while loop
-find . -type f -iname "*-proposed-baseline.png" -printf '%P\n' |
 
-while read line
-do
+function findfiles
+{
+	# pipe all proposed baseline PNG file names to the while loop
+	find . -type f -iname "*$1" -printf '%P\n' |
 
-	# remove the '.oscad' from the file name
-	baseName=${line::-22}
-	
-	targetName=$baseName".scad"
+	while read line
+	do
 
-	if   [ ! -f $targetName ] # test to see if the corresponding .scad file exists
-	then
-	
-		echo "missing input file: $targetName for a proposed baseline PNG: $line"
+		# remove the '.oscad' from the file name
+		baseName=${line::-$2}
+			
+		targetName=$baseName".scad"
+
+		if   [ ! -f $targetName ] # test to see if the corresponding .scad file exists
+		then
 		
-	fi	
-	
-done
+			echo "missing input file: $targetName for a proposed baseline PNG: $line"
+			
+		fi	
+		
+	done
+}
+
+# call the function
+findfiles -proposed-baseline.png 22
+
+# this next one does not work (as is) because the -baseline.png picks up the -propossed-baseline.png files as well :(
+#findfiles -baseline.png 13
