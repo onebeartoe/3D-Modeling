0fec0e56e8aee646f32a022eb333fd82dc413a3e
onebeartoe <onebeartoe@gmail.com>
2017-01-23 21:49:23 -0600

    These are the initial changes to select, persist, and restore the OpenSCAD file.

--- OpenSCAD/native-customizer/src/main/java/org/onebeartoe/modeling/openscad/customizer/FXMLController.java
+++ OpenSCAD/native-customizer/src/main/java/org/onebeartoe/modeling/openscad/customizer/FXMLController.java
@@ -13,31 +13,37 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import org.onebeartoe.application.DesktopApplication;
+import org.onebeartoe.application.JavaPreferencesService;
+import org.onebeartoe.application.PreferencesService;
+import org.onebeartoe.application.ui.GraphicalUserInterfaceServices;
+import org.onebeartoe.application.ui.JavaFxGuiServices;
 import org.onebeartoe.modeling.openscad.test.suite.PngGenerator;
 
-public class FXMLController implements Initializable 
+public class FXMLController implements Initializable, DesktopApplication 
 {
-    Logger logger;
+    private Logger logger;
+    
+    private PreferencesService preferencesService;
+    
+    private GraphicalUserInterfaceServices uiServices;
+    
+    private ThingiverseCustomizerService customizerService;
     
     private PngGenerator pg;
     
-    @FXML
-    private Label label;
+    private String openScadFile;
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) 
+//    @FXML
+//    private Label label;
+    
+    private void generateCutomizerFile()
     {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-        
-        String inpath = "C:\\home\\owner\\versioning\\github\\3D-Modeling\\OpenSCAD\\src\\main\\openscad\\office\\keyboard\\keycaps\\cherry-mx\\letter-key-test.scad";
-        inpath = "C:\\home\\owner\\versioning\\github\\3D-Modeling\\OpenSCAD\\src\\main\\openscad\\toys\\fidget-spinner\\fidget-spinner.scad";
-        
-        File infile = new File(inpath);
-        ThingiverseCustomizerService customizerService = new ThingiverseCustomizerService();
-
         try 
         {
+            File infile = new File(openScadFile);
             String interpolatedContent = customizerService.interpolateOpenScad(infile);
             String path = infile.getParent() + File.separator + infile.getName() + "-CUSTOMIZER.scad";
             File outfile = new File(path);
@@ -48,6 +54,32 @@ public class FXMLController implements Initializable
         catch (IOException  ex)
         {
             logger.log(Level.SEVERE, ex.getMessage(), ex);
+            
+            String message = "Error: " + ex.getMessage();
+            String title = "An Error Occurred";
+            uiServices.infoMessage(message, title);
+        }
+    }
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event)
+    {
+        System.out.println("You clicked me!");
+//        label.setText("Hello World!");
+        
+//        String inpath = "C:\\home\\owner\\versioning\\github\\3D-Modeling\\OpenSCAD\\src\\main\\openscad\\office\\keyboard\\keycaps\\cherry-mx\\letter-key-test.scad";
+//        inpath = "C:\\home\\owner\\versioning\\github\\3D-Modeling\\OpenSCAD\\src\\main\\openscad\\toys\\fidget-spinner\\fidget-spinner.scad";
+//        File infile = new File(inpath);
+
+        if(openScadFile == null)
+        {
+            String message = "No OpenSCAD file is selected.  Please select an OpenSCAD file to generate a Customizer version.";
+            String title = "Select an OpenSCAD File";
+            uiServices.infoMessage(message, title);
+        }
+        else
+        {
+            generateCutomizerFile();
         }
     }
     
@@ -56,5 +88,28 @@ public class FXMLController implements Initializable
     {
         logger = Logger.getLogger(getClass().getName() );
         pg = new PngGenerator();
-    }    
+        
+        preferencesService = new JavaPreferencesService(this);
+        String defaultValue = null;
+        openScadFile = preferencesService.get(NativeCustomizerPreferences.OPENSCAD_FILE, defaultValue);
+        
+        uiServices = new JavaFxGuiServices();
+        
+        customizerService = new ThingiverseCustomizerService();
+    }
+
+    @FXML 
+    void selectOpenScadFile(ActionEvent event)
+    {
+        // show the file chooser dialog
+        FileChooser fc = new FileChooser();
+        final Stage dialog = new Stage();
+        File file = fc.showOpenDialog(dialog);
+        if (file != null)
+        {
+            // save the current file to preferences
+            String preferenceName = NativeCustomizerPreferences.OPENSCAD_FILE.name();
+            preferencesService.saveProperty(preferenceName, file.getAbsolutePath() );
+        }
+    }
 }
---
+++ OpenSCAD/native-customizer/src/main/java/org/onebeartoe/modeling/openscad/customizer/NativeCustomizerPreferences.java
@@ -0,0 +1,13 @@
+/*
+
+ */
+package org.onebeartoe.modeling.openscad.customizer;
+
+/**
+ *
+ * @author Roberto Marquez <https://www.youtube.com/user/onebeartoe>
+ */
+public enum NativeCustomizerPreferences 
+{
+    OPENSCAD_FILE
+}
