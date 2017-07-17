2d1611f6fc7e1d9208a6b6908c3250bd06238d2e
onebeartoe <onebeartoe@gmail.com>
2017-07-17 13:43:04 -0500

    The inline tool now restores the window location and size between runs, with a little help from the org.onebeartoe.application.DesktopApplication interface.

--- OpenSCAD/inline-tool/src/main/java/org/onebeartoe/modeling/openscad/inliner/MainApp.java
+++ OpenSCAD/inline-tool/src/main/java/org/onebeartoe/modeling/openscad/inliner/MainApp.java
@@ -1,17 +1,53 @@
 
 package org.onebeartoe.modeling.openscad.inliner;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import org.onebeartoe.application.DesktopApplication;
+import org.onebeartoe.application.ui.WindowProperties;
 
-public class MainApp extends Application 
+public class MainApp extends Application implements DesktopApplication
 {
+    private WindowProperties wp;
+    
+    @Override
+    public void init()
+    {
+        String applicationId = getClass().getName();
+        try 
+        {
+            wp = restoreWindowProperties(applicationId);
+        } 
+        catch (IOException | ClassNotFoundException ex) 
+        {
+            ex.printStackTrace();
+        }
+        
+        if(wp == null)
+        {
+            wp = new WindowProperties();
+            
+            wp.id = getClass().getName();
+            wp.applicationName = getClass().getSimpleName();
+            
+            // use the default values
+            wp.width = 573;
+            wp.height = 114;
+            
+            wp.locationX = 50;
+            wp.locationY = 100;
+        }
+    }
+    
     @Override
     public void start(Stage stage) throws Exception 
     {
@@ -24,6 +60,35 @@ public class MainApp extends Application
         
         stage.setTitle("OpenSCAD Inliner");
         stage.setScene(scene);
+        
+        stage.setWidth(wp.width);
+        stage.setHeight(wp.height);
+                
+        stage.setX(wp.locationX);
+        stage.setY(wp.locationY);
+        
+        stage.setOnCloseRequest( new EventHandler<WindowEvent>() 
+        {
+            @Override
+            public void handle(WindowEvent event) 
+            {
+                System.out.println("bye");
+                wp.locationX = (int) stage.getX();
+                wp.locationY = (int) stage.getY();
+
+                wp.width = (int) stage.getWidth();
+                wp.height = (int) stage.getHeight();
+
+                try
+                {
+                    MainApp.this.persistWindowProperties(wp);
+                } 
+                catch (IOException ex) 
+                {
+                    ex.printStackTrace();
+                }
+            }
+        });
         stage.show();
     }
 
