84047d90dfe5d12525d5dfdec732376ad702c50b
onebeartoe <onebeartoe@gmail.com>
2017-08-30 12:39:18 -0500

    A test was added to verify job configuration.

---
+++ OpenSCAD/library/src/test/java/org/onebeartoe/modeling/openscad/test/suite/CleanMeTest.java
@@ -0,0 +1,19 @@
+/*
+
+ */
+package org.onebeartoe.modeling.openscad.test.suite;
+
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Roberto Marquez <https://www.youtube.com/user/onebeartoe>
+ */
+public class CleanMeTest 
+{
+    @Test
+    public void testB()
+    {
+        System.out.println("hi from test b");
+    }
+}
