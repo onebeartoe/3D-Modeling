e386cb66b1805beaa5435496b92819668448ee5c
Roberto Marquez <onebeartoe@gmail.com>
2017-11-29 15:01:50 -0600

    This is the first version of an OpenSCAD dialog bubble.

--- javascad/src/main/java/org/onebeartoe/modeling/javascad/shapes/dialog/bubble/DialogBubble.java
+++ javascad/src/main/java/org/onebeartoe/modeling/javascad/shapes/dialog/bubble/DialogBubble.java
@@ -7,7 +7,6 @@ import eu.printingin3d.javascad.models.Abstract3dModel;
 import eu.printingin3d.javascad.models.Complex3dModel;
 import eu.printingin3d.javascad.models.Extendable3dModel;
 import eu.printingin3d.javascad.tranzitions.Difference;
-import eu.printingin3d.javascad.tranzitions.Translate;
 import eu.printingin3d.javascad.tranzitions.Union;
 import org.onebeartoe.modeling.javascad.shapes.triangle.Triangle;
 import org.onebeartoe.modeling.javascad.shapes.open.oval.OpenOval;
@@ -18,51 +17,49 @@ import org.onebeartoe.modeling.javascad.shapes.open.oval.OpenOval;
 public class DialogBubble extends Extendable3dModel
 {
     public DialogBubble(double zLength)
-    {        
-        Complex3dModel
-//        Union
-//        Extendable3dModel
-                block = block(zLength);
+    {
+        Angles3d pointerRotate = Angles3d.zOnly(-10);
         
-        Extendable3dModel cutout = cutout(zLength);
+        Complex3dModel block = block(zLength, pointerRotate);
         
-        Difference difference = new Difference(block
-        //        , cutout
-        );
+        Abstract3dModel cutout = cutout(zLength, pointerRotate);
+        
+        Difference difference = new Difference(block, cutout);
         
         baseModel = difference;
     }
     
-    private Complex3dModel
-//                 Extendable3dModel
-         block(double zLength)
+    private Complex3dModel block(double zLength, Angles3d pointerRotate)
     {
         OpenOval bubble = new OpenOval(1, zLength);
         
-        double sideLength = 3;
-        Abstract3dModel pointer = new Triangle(zLength, sideLength);
-        
-//        pointer.rotate(Angles3d.zOnly(45));
-        Coords3d translateCoords = new Coords3d(6, -3, 0);
-        
-        pointer = pointer.rotate(Angles3d.zOnly(-10))        
-                         .move(translateCoords);
-                         
-        
-//        Translate translate = new Translate(pointer, translateCoords);
-        
+        Abstract3dModel pointer = pointer(zLength, pointerRotate);
+
         Union block = new Union(bubble, pointer);
-//        Union block = new Union(bubble, translate);
-        
-//        block.
         
         return block;
     }
     
-    private Extendable3dModel cutout(double height)
+    private Abstract3dModel cutout(double height, Angles3d pointerRotate)
     {
-        Triangle pointer = new Triangle(height, 5);
+        Coords3d translateCoords = new Coords3d(-1.8, 0.3, -0.01);
+        
+        Abstract3dModel pointer = pointer(height+0.03, pointerRotate)
+                                    .move(translateCoords)
+        ;
         
         return pointer;
-    }    
+    }
+
+    private Abstract3dModel pointer(double zLength, Angles3d pointerRotate)
+    {
+        double sideLength = 3;
+        Coords3d translateCoords = new Coords3d(6, -3, 0);
+        
+        Abstract3dModel pointer = new Triangle(zLength, sideLength)
+                                        .rotate(pointerRotate)        
+                                        .move(translateCoords);
+
+        return pointer;
+    }
 }
