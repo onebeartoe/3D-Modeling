0624c590b4ad77f1b5da255ee77169c6335102fd
Roberto Marquez <onebeartoe@gmail.com>
2017-11-30 13:43:42 -0600

    The baseline images were updated.

---
+++ javascad/src/test/java/org/onebeartoe/modeling/javascad/ModuleDeclaration.java
@@ -0,0 +1,12 @@
+
+package org.onebeartoe.modeling.javascad;
+
+import eu.printingin3d.javascad.models.Complex3dModel;
+
+/**
+ * @author Roberto Marquez
+ */
+public class ModuleDeclaration //extends Complex3dModel
+{
+    
+}
