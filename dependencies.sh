8d4b50c923a0b1b52a9e2c2af559de319f6ef6b0
Roberto Marquez <onebeartoe@gmail.com>
2017-12-21 09:21:19 -0600

    The Travis configuration was updated to install OpenSCAD and ImageMagick.

---
+++ dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ev
+
+sudo add-apt-repository --yes ppa:openscad/releases
+sudo apt-get update -qq
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openscad imagemagick xvfb
