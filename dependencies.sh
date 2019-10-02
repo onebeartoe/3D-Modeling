40faf7ef6efd06933f507c621db089768a07e97b
onebeartoe <onebeartoe@gmail.com>
2019-10-02 13:06:52 -0500

    add openscad-mcad as a dependency

--- dependencies.sh
+++ dependencies.sh
@@ -6,6 +6,8 @@ sudo add-apt-repository --yes ppa:openscad/releases
 sudo apt-get update -qq
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openscad imagemagick xvfb
 
+sudo apt-get install -y openscad-mcad
+
 which openscad
 whereis openscad
 
