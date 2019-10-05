6cdd0f855342e59badea14c1f86df4fdd52b5d48
onebeartoe <onebeartoe@gmail.com>
2019-10-05 12:26:37 -0500

    address null pointer exception

--- openscad/test-suite/src/test/java/org/onebeartoe/modeling/openscad/test/suite/utils/OpenScadTestSuiteServiceSpecification.java
+++ openscad/test-suite/src/test/java/org/onebeartoe/modeling/openscad/test/suite/utils/OpenScadTestSuiteServiceSpecification.java
@@ -1,12 +1,12 @@
 
 package org.onebeartoe.modeling.openscad.test.suite.utils;
 
-//import org.testng.annotations.BeforeMethod;
 import java.io.IOException;
+import java.util.ArrayList;
 import org.onebeartoe.modeling.openscad.test.suite.model.GeneratePngBaselineResults;
 import org.onebeartoe.modeling.openscad.test.suite.model.RunProfile;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 /**
@@ -16,17 +16,18 @@ public class OpenScadTestSuiteServiceSpecification
 {
     private OpenScadTestSuiteService implementation;
 
-    @BeforeMethod
+    @BeforeClass
     public void initializeTest()
     {
         implementation = new OpenScadTestSuiteService();
     }
 
-    
     @Test
     public void generateProposedBaselines() throws IOException, InterruptedException
     {
         RunProfile runProfile = new RunProfile();
+        
+        runProfile.openscadPaths = new ArrayList();
         
         GeneratePngBaselineResults results = implementation.generateProposedBaselines(runProfile);
         
