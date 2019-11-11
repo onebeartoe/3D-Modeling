9389a229641d324128a8c1be3951e570f9f7d423
onebeartoe <onebeartoe@gmail.com>
2019-11-10 20:35:30 -0600

    use a sysout logger and remove the TestNG grouping

--- openscad/opensacd-services/src/test/java/org/onebeartoe/modeling/opensacd/services/ThingiverseCustomizerServiceSpecification.java
+++ openscad/opensacd-services/src/test/java/org/onebeartoe/modeling/opensacd/services/ThingiverseCustomizerServiceSpecification.java
@@ -3,9 +3,12 @@ package org.onebeartoe.modeling.opensacd.services;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Logger;
 import org.apache.commons.lang3.StringUtils;
+import org.onebeartoe.application.logging.SysoutLoggerFactory;
 import static org.testng.AssertJUnit.assertTrue;
 import org.testng.annotations.Test;
+//import org.testng.log4testng.Logger;
 
 /**
  *
@@ -16,21 +19,30 @@ public class ThingiverseCustomizerServiceSpecification
     private ThingiverseCustomizerService implementation;
     
     private String clientPath = "src/test/resources/client.scad";
+    
+    private Logger logger;
             
     public ThingiverseCustomizerServiceSpecification() throws IOException
     {
         implementation = new ThingiverseCustomizerService();
+        
+        logger = SysoutLoggerFactory.getLogger( getClass().getName() );
+        
+//        logger = Logger.getLogger( getClass().getName() );
+//        logger.setUseParentHandlers(false);
     }
     
     @Test
     public void generateCustomizerFile() throws IOException
     {
+logger.info(">some mesage<");
+System.out.println(">plooop<");
         File openScadInfile = new File(clientPath);
         
         File outfile = implementation.generateCustomizerFile(openScadInfile);
                 
         assertTrue( outfile.exists() );
-        
+logger.severe("it's fatal!");
         String orignalName = openScadInfile.getName();
         String expectedName = orignalName + "-inlined.scad";        
         String actualName = outfile.getName();
@@ -38,7 +50,7 @@ public class ThingiverseCustomizerServiceSpecification
         assertTrue( expectedName.equals(actualName) );
     }
 
-    @Test(groups = "unit-tests")
+    @Test
     public void whenIncludesExistThenAllArePresentInInlined() throws IOException 
     {
         File scadFile = new File(clientPath);
