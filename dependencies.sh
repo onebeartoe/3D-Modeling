2228f016d1e56d2020fa579066f619149e0dd5d4
Roberto Marquez <onebeartoe@gmail.com>
2019-11-29 16:27:00 -0600

    add sudo to the cp command

--- dependencies.sh
+++ dependencies.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-cp openscad.list /etc/apt/sources.list.d/
+sudo cp openscad.list /etc/apt/sources.list.d/
 ls -la /etc/apt/sources.list.d/openscad.list
 
 sudo add-apt-repository --yes ppa:openscad/releases
