57917c9913fba68db6623dd7d6c583c6ad8932ce
Roberto Marquez <onebeartoe@gmail.com>
2019-11-30 15:34:53 -0600

    cleanup

--- dependencies.sh
+++ dependencies.sh
@@ -3,7 +3,7 @@
 set -ev
 
 sudo cp openscad.list /etc/apt/sources.list.d/
-#ls -la /etc/apt/sources.list.d/openscad.list
+ls -la /etc/apt/sources.list.d/openscad.list
 
 wget -qO - https://files.openscad.org/OBS-Repository-Key.pub | sudo apt-key add -
 
@@ -14,29 +14,16 @@ sudo add-apt-repository --yes ppa:openscad/releases
 sudo apt-get update -qq
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openjfx
-#sudo snap install openscad-nightly
 
-#sudo DEBIAN_FRONTEND=noninteractive apt-get install -y imagemagick xvfb
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openscad-nightly imagemagick xvfb
 
-#ls -la /usr/bin
-#locate openscad-nightly
-
-#TODO: renable this once night is working
 sudo apt-get install -y openscad-mcad
 
-#TODO: renable this once night is working
-#which openscad-nightly
-#TODO: renable this once night is working
+which openscad-nightly
+
 whereis openscad-nightly
 
-#TODO: renable this once night is working
 whereis openscad-mcad
 
-#ls -la /usr/bin/openscad-nightly
-
-#ls -la /usr/share/openscad-nightly
-
-#TODO: renable this once night is working
 ls -la /usr/share/openscad-nightly/
 ls -la /usr/share/openscad-nightly/libraries
