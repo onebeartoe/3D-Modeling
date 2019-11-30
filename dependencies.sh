adbecc3dbe34954f9f5381b155efad3180357a4e
Roberto Marquez <onebeartoe@gmail.com>
2019-11-30 13:59:52 -0600

    use the ppa:openscad apt repository

--- dependencies.sh
+++ dependencies.sh
@@ -10,7 +10,7 @@ wget -qO - https://files.openscad.org/OBS-Repository-Key.pub | sudo apt-key add 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 40976EAF437D05B5
 
-#sudo add-apt-repository --yes ppa:openscad/releases
+sudo add-apt-repository --yes ppa:openscad/releases
 sudo apt-get update -qq
 
 #sudo DEBIAN_FRONTEND=noninteractive apt-get install -y snapd
